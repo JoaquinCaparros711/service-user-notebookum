2bb8d1ce3cef18705dd99fb028082e0856cf7343
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2026-04-28 13:33:07 -0300

    #1 Created basic structure

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    @app.route("/health")
+    def health_check():
+        return {"status": "ok", "service": "user"}, 200
+
+    return app
